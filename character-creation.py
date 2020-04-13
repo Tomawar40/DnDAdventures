cdd51e8fd8d8b61d6dfbeef2f28dfcfcd8b5ebfe
Marslew <35205152+Marslew@users.noreply.github.com>
2020-04-12 22:57:24 -0700

    Create character file
    This is a VERY rough draft for character creation. Written to give guidance to the Smith role in the project.

---
+++ character-creation.py
@@ -0,0 +1,37 @@
+import pygame
+
+print("Welcome to the character generator!")
+print("What kind of Character would you like to make?)
+print("Please list your preference, with the number attached.)
+print(" 1. Fighter")
+print(" 2. Rogue")
+print(" 1. Wizard")
+
+print("What Race would you like to be?")
+print("You could be flexible like a human, agile like an elf, or hearty as a dwarf. Pick which you'd prefer.")
+
+#TODO Take the user choice and code in what would result from this choice. Include racial bonuses, features, and any other
+# impact to their character
+
+print("Excellent. Now we will roll your attribute scores. These will affect how well your character operates in this world.")
+
+#TODO Import the stats generation script. Also, depending on the CLASS chosen, give the user a choice between the "preferred" option
+# or to go their own path. 
+
+
+#TODO Designate the points from generation into attribute scores along with the racial bonuses. 
+
+
+#TODO Generate Hit Points based off the designation of attribute scores
+
+#TODO Generate Initial Skill Scores. As a bonus, incorporate proficency bonus
+
+#TODO Make the user selec character details like faith, physical descriptors, height/weight/etc. 
+
+#TODO Make the user select a background FROM Entertainer, Hermit, Urchin. 
+
+#TODO Make the user craft the equipment choices for the initial character based off the 5th edition layout. 
+
+#TODO Write the charachter choices to a file for permenance. 
+
+#
